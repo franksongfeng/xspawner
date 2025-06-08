d60139c0d20f652d6ed5b9dc67ffcefce5b028c2
Frank Song <43217183@qq.com>
2025-06-08 14:21:56 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,15 +3,17 @@
 # Copyright © 2025 Song Feng.
 
 # download xspawner
-sudo rm xspawner -rf
-sudo git clone https://github.com/franksongfeng/xspawner
+# sudo rm xspawner -rf
+# sudo git clone https://github.com/franksongfeng/xspawner
+
 cd xspawner
 
 export SERVER_IP=127.0.0.1
 export SERVER_PORT=8000
 export SERVER_NAME=root
+
 # start xspawner daemon and root app
 nohup sudo python3 -u -m xspawner --name $SERVER_NAME --app spawner --host $SERVER_IP --port $SERVER_PORT --severity info &
 
-# test the prior app
+# test xspawner
 sudo python3 -m xspawner.apps.spawner.tests http://$SERVER_IP:$SERVER_PORT
