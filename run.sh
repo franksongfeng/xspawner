51e24659808e1cd8877b602aa040a256d9815783
franksongfeng <43217183@qq.com>
2026-01-24 16:13:00 +0800

    enhance run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,12 @@
 # -*- coding: utf-8 -*-
 # Copyright © 2025 Song Feng.
 
+# check parameters
+if [ $# -ne 4 ]; then
+    echo "Err: The script needs 4 paramters: name, app, host, port"
+    exit 1
+fi
+
 # start spawner
 nohup sudo python3 -u -m xspawner --name $1 --app $2 --host $3 --port $4 --severity debug &
 
