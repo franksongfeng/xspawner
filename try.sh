f5e0800b0590a053523ea0f0f4a9cefe44b7c2d9
Frank Song <43217183@qq.com>
2026-02-11 22:28:39 +0800

    Create try.sh

---
+++ try.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# Copyright © 2025 Song Feng.
+
+# check parameters
+if [ $# -ne 4 ]; then
+    echo "Err: The script needs 4 paramters: name, app, host, port"
+    exit 1
+fi
+
+# start spawner
+sudo python3 -u -m xspawner --name $1 --app $2 --host $3 --port $4 --severity debug
