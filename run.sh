27911deb3b0d0a26a002b60fdf6ecf1a9f07ed69
franksongfeng <43217183@qq.com>
2026-03-04 17:29:41 +0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -12,17 +12,17 @@ DAEMON="false"
 
 if [ $# -eq 6 ]; then
     # ssl
-    SCHEME="http"
-    CMD="sudo python3 -u -m xspawner --name $1 --app $2 --host $3 --port $4 --severity debug"
-else
-    # no ssl
     SCHEME="https"
     CMD="sudo python3 -u -m xspawner --name $1 --app $2 --host $3 --port $4 --severity debug  --ssl --certfile $5 --keyfile $6"
+else
+    # no ssl
+    SCHEME="http"
+    CMD="sudo python3 -u -m xspawner --name $1 --app $2 --host $3 --port $4 --severity debug"
 fi
 
 if [ "$DAEMON" = "true" ]; then
     # daemon
-    REAL_CMD ="nohup "$CMD" &"
+    REAL_CMD="nohup "$CMD" &"
     $REAL_CMD
     sleep 1
     sudo python3 -m xspawner.apps.spawner.tests $SCHEME://$3:$4
