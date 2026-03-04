34f458038b8a820fb9d5e4dbeba0e715449ecd2b
franksongfeng <43217183@qq.com>
2026-03-04 14:42:06 +0800

    Improve run.sh
    remove try.sh

--- run.sh
+++ run.sh
@@ -3,14 +3,26 @@
 # Copyright © 2025 Song Feng.
 
 # check parameters
-if [ $# -ne 4 ]; then
-    echo "Err: The script needs 4 paramters: name, app, host, port"
+if [ $# -lt 4 ]; then
+    echo "Err: At least 4 parameters are required: name, app, host, port"
     exit 1
 fi
 
-# start spawner
-nohup sudo python3 -u -m xspawner --name $1 --app $2 --host $3 --port $4 --severity debug &
+DAEMON="false"
 
-# test spawner
-sleep 1
-sudo python3 -m xspawner.apps.spawner.tests http://$3:$4
+if [ $# -eq 6 ]; then
+    SCHEME="http"
+    CMD="sudo python3 -u -m xspawner --name $1 --app $2 --host $3 --port $4 --severity debug"
+else
+    SCHEME="https"
+    CMD="sudo python3 -u -m xspawner --name $1 --app $2 --host $3 --port $4 --severity debug  --ssl --certfile $5 --keyfile $6"
+fi
+
+if [ "$DAEMON" = "true" ]; then
+    REAL_CMD ="nohup "$CMD" &"
+    $REAL_CMD
+    sleep 1
+    sudo python3 -m xspawner.apps.spawner.tests $SCHEME://$3:$4
+else
+    $CMD
+fi
